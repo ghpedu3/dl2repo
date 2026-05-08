e00f657540ee6d161c3be00a1e04002ada8d85b8
ghpedu3 <164553489+ghpedu3@users.noreply.github.com>
2026-05-08 18:19:40 +0000

    Update downloader.sh

--- scripts/downloader.sh
+++ scripts/downloader.sh
@@ -293,6 +293,7 @@ _TCMD_BASE_NAME="tcmd-${_TCMD_VERSION}"
 
     # wget --continue "https://jpsoft.com/all-downloads/all-downloads.html"
     # wget --user-agent="Mozilla/5.0" --continue --content-disposition "https://en.wikipedia.org/api/rest_v1/page/pdf/TCP_hole_punching"
+    wget "https://download.sysinternals.com/files/SysinternalsSuite.zip"
     
 
     cd "${_ARTIFACTS}"
