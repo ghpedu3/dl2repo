1dfceb48b88588e05d2524a29008bdc5cebebe62
ghpedu3 <164553489+ghpedu3@users.noreply.github.com>
2026-05-14 02:01:36 +0000

    Update downloader.sh - fix `mirror_site_wget`

--- scripts/downloader.sh
+++ scripts/downloader.sh
@@ -102,7 +102,7 @@ mirror_site_wget() {
 	    echo "<html>"
         echo "<head>"
 	    echo "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" >"
-	    echo "    <meta HTTP-EQUIV="\Refresh\" CONTENT=\"0; URL=./${url_path}/index.html\" >"
+	    echo "    <meta HTTP-EQUIV=\"Refresh\" CONTENT=\"0; URL=./${url_path}/index.html\" >"
 	    echo "    <title>${MNAME}</title>"
         echo "</head>"
         echo "<!--"
