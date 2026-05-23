a82499427459c396469991a3e4cb1eb9778411fb
ghpedu3 <164553489+ghpedu3@users.noreply.github.com>
2026-05-23 08:01:08 +0000

    Update downloader.sh

--- scripts/downloader.sh
+++ scripts/downloader.sh
@@ -7,7 +7,7 @@ __ENABLE_DOWNLOAD_DENO="false"
 __ENABLE_DOWNLOAD_ELECTRON="false"
 __ENABLE_DOWNLOAD_QJSNG="false"
 __ENABLE_DOWNLOAD_BUN="false"
-__ENABLE_DOWNLOAD_BUN_CANARY="false"
+__ENABLE_DOWNLOAD_BUN_CANARY="true"
 __ENABLE_DOWNLOAD_BBW32="false"
 __ENABLE_DOWNLOAD_W64DEVKIT="false"
 __ENABLE_DOWNLOAD_GOLANG="false"
@@ -455,7 +455,7 @@ _BUN_BASE_NAME="bun-v${_BUN_VERSION}"
 } 2>&1 | tee "${_ARTIFACTS}/stderr.txt" || echo 'BUN download was disabled' >>"${_ARTIFACTS}/stderr.txt"
 
 
-_BUN_CANARY_COMMIT="30c007120807d4576417b897f86e22f96e45d939"
+_BUN_CANARY_COMMIT="3c92be850ac3ddb5b2ac020838b6b603f9e724cd"
 _BUN_CANARY_BASE_URL="https://github.com/oven-sh/bun/releases/download/canary"
 _BUN_CANARY_BASE_NAME="bun-canary-${_BUN_CANARY_COMMIT}"
 [ "${__ENABLE_DOWNLOAD_BUN_CANARY}" = "true" ] && {
