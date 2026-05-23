a1649b0ed2db24213fb1a3e1fe6e0295ad148e10
ghpedu3 <164553489+ghpedu3@users.noreply.github.com>
2026-05-23 18:02:29 +0000

    Update downloader.sh

--- scripts/downloader.sh
+++ scripts/downloader.sh
@@ -956,7 +956,7 @@ _SUMATRA_PDF_BASE_NAME="sumatrapdf-${_SUMATRA_PDF_VERSION}"
     # my_wget "https://en.wikipedia.org/api/rest_v1/page/pdf/TCP_hole_punching" ""  --no-verbose --user-agent="Mozilla/5.0" --content-disposition
     # my_wget "https://en.wikipedia.org/api/rest_v1/page/pdf/JSONP" ""  --no-verbose --user-agent="Mozilla/5.0" --content-disposition
 
-    get_en_wikipedia_pdf JSON_Web_Signature JSON_Web_Encryption Single_sign-on Identity_provider API_key Access_token Basic_access_authentication Digest_access_authentication Claims-based_identity CBOR Web_storage
+    # get_en_wikipedia_pdf JSON_Web_Signature JSON_Web_Encryption Single_sign-on Identity_provider API_key Access_token Basic_access_authentication Digest_access_authentication Claims-based_identity CBOR Web_storage
 
     # my_wget "https://download.sysinternals.com/files/SysinternalsSuite.zip"
     # my_wget "https://download.sysinternals.com/files/PSTools.zip"
