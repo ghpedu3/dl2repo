e7838922fcc32556d2f327d5da370f1b90bff3a8
ghpedu3 <164553489+ghpedu3@users.noreply.github.com>
2026-05-25 06:47:00 +0000

    Update scratch.sh

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -1,6 +1,8 @@
 
 # scratch
 
-return 0
+ # return 0
 
-my_wget 'https://codeberg.org/forgejo/forgejo/releases/'
+git clone --mirror 'https://codeberg.org/forgejo/forgejo.git' && pushd forgejo.git && git repack -a -d -f
+popd
+archive_folder forgejo.git
